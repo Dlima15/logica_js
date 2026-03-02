eea94d4a32df24a458774be16f41d21178b2b15e
Dlima15 <danilo.lima7456@gmail.com>
2026-03-02 14:55:21 -0300

    ex01

---
+++ desafio/ex001.js
@@ -0,0 +1,11 @@
+//Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+console.log("ok");
+
+let dia = prompt("Qual o dia da semana?");
+
+if (dia == "sabado" || dia == "domingo"){
+    console.log("Bom fim de semana")
+} else{
+    console.log("Boa semana bom trabalho")
+}
