db7fc9475e6b55b6ce7c1b960bbaca99a041c650
Dlima15 <danilo.lima7456@gmail.com>
2026-03-02 14:13:03 -0300

    concatenação aplicada

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 12;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
 
 // se chute for igual ao numero secreto
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai você descobriu o número secreto ${numeroSecreto}`);
 } else {
     alert("Vc errou, atualize a pagina e tente novamente, oss!")
 }
+
