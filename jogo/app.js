3c5327c6ec94873aa87881ded0cfba15867547cb
Dlima15 <danilo.lima7456@gmail.com>
2026-03-03 23:42:54 -0300

    a

--- jogo/app.js
+++ jogo/app.js
@@ -1,13 +1,25 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 12;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
 
+let chute;
+let tentativas = 1; 
 
-// se chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert("Vc errou, atualize a pagina e tente novamente, oss!")
+// Enquanto o chute não for igual ao numero 
+while (chute != numeroSecreto){
+    chute = prompt("Advinhe o número secreto: de 1 a 10");
+
+    // se chute for igual ao numero secreto
+    if (chute = numeroSecreto) {
+        alert(`Isso ai você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {        
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        
+        // numero de tentativas somadas
+        tentativas++; 
+    }
 }
-
